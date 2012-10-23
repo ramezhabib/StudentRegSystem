9cc366c370cdc0f0bac322b622048c75ea0ab683
Ramez Habib <ramezhabib@gmail.com>
2012-10-22 22:50:02 -0400

    added test class

---
+++ StudentSystem/src/edu/wayne/csc4997/studentSystem/model/TestClass.java
@@ -0,0 +1,21 @@
+package edu.wayne.csc4997.studentSystem.model;
+
+public class TestClass {
+	
+	public String name;
+	public String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+
+}
